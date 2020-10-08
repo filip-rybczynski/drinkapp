82aa283c54c7f12f9d71feba2a36ce4486122c95
Filip Rybczyński <filipryba@gmail.com>
2020-10-08 23:58:05 +0200

    feat: record value counter

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -11,47 +11,55 @@ const counterMax = document.querySelector(".max--js");
 const addButton = document.querySelector(".add--js");
 const removeButton = document.querySelector(".remove--js");
 
-const key = new Date().toISOString().slice(0,10);
+const key = new Date().toISOString().slice(0, 10);
 
 const entry = localStorage.getItem(key);
 
 if (entry) {
-    counterValue.innerHTML = entry;
+  counterValue.innerHTML = entry;
 }
 
-const entryRecord = localStorage.getItem('record');
+const entryRecord = localStorage.getItem("record");
 
 if (entryRecord) {
-counterMax.innerHTML = entryRecord;
+  counterMax.innerHTML = entryRecord;
 }
 
-    if (counterValue.innerHTML > 9) {
-        counterValue.style.fontSize = "6.2em"
-    }
+if (counterValue.innerHTML > 9) {
+  counterValue.style.fontSize = "6.2em";
+}
 
 addButton.addEventListener("click", () => {
-    counterValue.innerHTML++;
-    localStorage.setItem(key, counterValue.innerHTML);
-    if (counterValue.innerHTML == 10) {
-        counterValue.style.fontSize = "6.2em"
-    }
-
-    if (counterMax.innerHTML < counterValue.innerHTML) {
-        console.log('test');
-        counterMax.innerHTML = counterValue.innerHTML;
-        
-        localStorage.setItem('record', counterMax.innerHTML);
-    }
+  counterValue.innerHTML++;
+  localStorage.setItem(key, counterValue.innerHTML);
 
 
-})
+  if (entryRecord < counterValue.innerHTML) {
+      console.log("test");
+    counterMax.innerHTML = counterValue.innerHTML;
+
+    localStorage.setItem("record", counterMax.innerHTML);
+  }
+
+  if (counterValue.innerHTML == 10) {
+    counterValue.style.fontSize = "6.2em";
+  }
+});
 
 removeButton.addEventListener("click", () => {
-    if(counterValue.innerHTML > 0) {
-        counterValue.innerHTML--;
-        localStorage.setItem(key, counterValue.innerHTML);
-        if (counterValue.innerHTML == 9) {
-            counterValue.style.fontSize = "9em"
-        }
+  if (counterValue.innerHTML > 0) {
+    counterValue.innerHTML--;
+    localStorage.setItem(key, counterValue.innerHTML);
+
+
+    if (entryRecord < counterValue.innerHTML) {
+      counterMax.innerHTML = counterValue.innerHTML;
+
+      localStorage.setItem("record", counterMax.innerHTML);
     }
-})
+
+    if (counterValue.innerHTML == 9) {
+        counterValue.style.fontSize = "9em";
+      }
+  }
+});
