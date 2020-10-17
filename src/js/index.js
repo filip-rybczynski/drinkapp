fcacda30c9be2737621776f3b1fc22fc5c5994ce
Filip Rybczyński <filipryba@gmail.com>
2020-10-17 14:25:21 +0200

    fix: counter and record works correctly

--- src/js/index.js
+++ src/js/index.js
@@ -11,16 +11,37 @@ const counterMax = document.querySelector(".max--js");
 const addButton = document.querySelector(".add--js");
 const removeButton = document.querySelector(".remove--js");
 
-// const today = new Date();
-// const yesterday = new Date(today);
-// yesterday.setDate(yesterday.getDate() - 1);
+const today = new Date();
+const yesterday = new Date(today);
+yesterday.setDate(yesterday.getDate() - 1);
 
-const key = new Date().toLocaleString().slice(0, 10);
+const key = today.toLocaleString().slice(0, 10);
+
+const yesterKey = yesterday.toLocaleString().slice(0, 10);
+
 
 let currentValue = localStorage.getItem(key);
+let yesterdayValue = localStorage.getItem(yesterKey);
+let currentRecord = localStorage.getItem("record");
 
 if (currentValue) {
   counterValue.innerHTML = currentValue;
+}
+
+if (yesterdayValue) {
+  if (yesterdayValue > currentRecord) {
+    currentRecord = yesterdayValue;
+    localStorage.setItem("record", currentRecord);
+  }
+} else {
+  currentRecord = 0;
+  localStorage.setItem("record", currentRecord);
+}
+
+if (currentValue > currentRecord) {
+  counterMax.innerHTML = currentValue; 
+} else {
+  counterMax.innerHTML = currentRecord; 
 }
 
 if (currentValue > 9) {
@@ -31,6 +52,10 @@ addButton.addEventListener("click", () => {
   counterValue.innerHTML = ++currentValue;
   localStorage.setItem(key, currentValue);
 
+  if (currentValue > currentRecord) {
+    counterMax.innerHTML = currentValue;
+  }
+
   if (currentValue == 10) {
     counterValue.style.fontSize = "6.2em";
   }
@@ -40,6 +65,10 @@ removeButton.addEventListener("click", () => {
   if (currentValue > 0) {
     counterValue.innerHTML = --currentValue;
     localStorage.setItem(key, currentValue);
+  }
+
+  if (currentValue >= currentRecord) {
+    counterMax.innerHTML = currentValue;
   }
 
   if (currentValue == 9) {
