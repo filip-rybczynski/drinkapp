bf2e201c10bdc2c18f1d98b3e8e4b0079fe5fed0
Filip Rybczyński <filipryba@gmail.com>
2020-10-17 13:41:05 +0200

    initial commit - improving JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
@@ -17,71 +17,32 @@ const removeButton = document.querySelector(".remove--js");
 
 const key = new Date().toLocaleString().slice(0, 10);
 
-const entry = localStorage.getItem(key);
+let currentValue = localStorage.getItem(key);
 
-if (entry) {
-  counterValue.innerHTML = entry;
+if (currentValue) {
+  counterValue.innerHTML = currentValue;
 }
 
-let entryRecord = '';
-
-if(localStorage.getItem("record")) {
-    entryRecord = localStorage.getItem("record");
-} else {
-    entryRecord = 0;
-}
-
-  counterMax.innerHTML = entryRecord;
-
-if (counterValue.innerHTML > 9) {
-  counterValue.style.fontSize = "6.2em";
-}
-
+// if (counterValue.innerHTML > 9) {
+//   counterValue.style.fontSize = "6.2em";
+// }
 
 addButton.addEventListener("click", () => {
-  let boop;
-  boop = counterValue.innerHTML++;
-  console.log(boop);
-  console.log(counterValue.innerHTML);
-  localStorage.setItem(key, counterValue.innerHTML);
+  counterValue.innerHTML = ++currentValue;
+  localStorage.setItem(key, currentValue);
 
-  if (entryRecord < counterValue.innerHTML) {
-      console.log(entryRecord);
-      console.log(counterValue.innerHTML);
-      console.log(counterMax.interHTML);
-    counterMax.innerHTML = counterValue.innerHTML;
-
-    localStorage.setItem("record", counterMax.innerHTML);
-  }
-
-  if (counterValue.innerHTML == 10) {
-    counterValue.style.fontSize = "6.2em";
-  }
+  // if (counterValue.innerHTML == 10) {
+  //   counterValue.style.fontSize = "6.2em";
+  // }
 });
 
 removeButton.addEventListener("click", () => {
-  if (counterValue.innerHTML > 0) {
-    counterValue.innerHTML--;
-    localStorage.setItem(key, counterValue.innerHTML);
-
-
-    if (entryRecord <= counterValue.innerHTML) {
-      counterMax.innerHTML = counterValue.innerHTML;
-
-      localStorage.setItem("record", counterMax.innerHTML);
-    }
-
-    if (counterValue.innerHTML == 9) {
-        counterValue.style.fontSize = "9em";
-      }
+  if (currentValue > 0) {
+    counterValue.innerHTML = --currentValue;
+    localStorage.setItem(key, currentValue);
   }
-});
 
-// let today = new Date();
-// const test = today.toISOString().slice(0,10);
-// console.log(test);
-// const yesterday = new Date() - 1;
-let test = new Date();
-console.log(test);
-test = test.toISOString().slice(0, 10);
-console.log(test);
+  // if (counterValue.innerHTML == 9) {
+  //     counterValue.style.fontSize = "9em";
+  //   }
+});
