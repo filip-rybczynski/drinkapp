8ebd85a82d926957087de6df5c10ecd1c5bf72e5
Filip Rybczyński <filipryba@gmail.com>
2020-10-19 01:30:09 +0200

    Record is compared to latest daily value and larger value is saved to localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -6,20 +6,17 @@ registerSW();
 
 /* place your code below */
 
-function adjustOpacity(currentValue, currentRecord)
- {
-
-  if(currentValue == 0) return 0.3;
+function adjustOpacity(currentValue, currentRecord) {
+  if (currentValue == 0) return 0.3;
   else if (currentValue >= currentRecord) return 1;
   else {
-    const fraction = currentValue/currentRecord;
-  
-    let newOpacity = 0.3 + 0.7*fraction;
+    const fraction = currentValue / currentRecord;
+
+    let newOpacity = 0.3 + 0.7 * fraction;
 
     if (newOpacity > 1) newOpacity = 1;
-  
-    return newOpacity.toFixed(2);
 
+    return newOpacity.toFixed(2);
   }
 }
 
@@ -32,32 +29,47 @@ const removeButton = document.querySelector(".remove--js");
 const liquidGraphic = document.querySelector(".image--js");
 
 const today = new Date();
-const yesterday = new Date(today);
-yesterday.setDate(yesterday.getDate() - 1);
-
 const key = today.toLocaleString().slice(0, 10);
 
-const yesterKey = yesterday.toLocaleString().slice(0, 10);
-
-
-
 let currentValue = localStorage.getItem(key);
-let yesterdayValue = localStorage.getItem(yesterKey);
 let currentRecord = localStorage.getItem("record");
-
-
 
 if (currentValue) {
   counterValue.innerHTML = currentValue;
 } else {
   currentValue = 0;
+  localStorage.setItem(key, currentValue);
 }
 
 // SETTING RECORD:
 
-if (yesterdayValue) {
-  if (yesterdayValue > currentRecord) {
-    currentRecord = yesterdayValue;
+const allKeys = Object.keys(localStorage);
+
+if (currentRecord && allKeys.length > 3) {
+  /*Conditions check if record exists and if there are more than 3 elements in localStorage i.e. if there are 
+  keys for other dates than the current day (otherwise there will be an endless loop) */
+
+  let latest = new Date(today);
+  console.log(latest);
+  let latestValue = null;
+  console.log(latestValue);
+
+  do {
+    latest.setDate(latest.getDate() - 1);
+
+    console.log(latest);
+
+    const latestKey = latest.toLocaleString().slice(0, 10);
+
+    console.log(latestKey);
+
+    latestValue = localStorage.getItem(latestKey);
+
+    console.log(latestValue);
+  } while (latestValue == null);
+
+  if (latestValue > currentRecord) {
+    currentRecord = latestValue;
     localStorage.setItem("record", currentRecord);
   }
 } else {
@@ -75,12 +87,9 @@ if (currentValue > 9) {
   counterValue.style.fontSize = "6.2em";
 }
 
-
 liquidGraphic.style.opacity = adjustOpacity(currentValue, currentRecord);
 
-
 addButton.addEventListener("click", () => {
-
   counterValue.innerHTML = ++currentValue;
   localStorage.setItem(key, currentValue);
 
@@ -93,12 +102,10 @@ addButton.addEventListener("click", () => {
   }
 
   liquidGraphic.style.opacity = adjustOpacity(currentValue, currentRecord);
-
 });
 
 removeButton.addEventListener("click", () => {
   if (currentValue > 0) {
-
     counterValue.innerHTML = --currentValue;
     localStorage.setItem(key, currentValue);
 
